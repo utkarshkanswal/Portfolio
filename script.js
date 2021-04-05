9010ad1765694c51bcea15a99442bc53689bcf88
Utkarsh Kumar <utkarshkumar965@gmail.com>
2021-04-05 11:55:35 +0530

    Updated Website

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLink = document.getElementsByClassName('navbar-link')[0];
+toggleButton.addEventListener('click', () => {
+    navbarLink.classList.toggle('active');
+});
