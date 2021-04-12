7f017aec175656f4dc8c00434ae250096ec687a6
Utkarsh Kumar <utkarshkumar965@gmail.com>
2021-04-12 13:00:42 +0530

    Updated nav bar

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const navbarLink = document.getElementsByClassName('navbar-link')[0];
 toggleButton.addEventListener('click', () => {
     navbarLink.classList.toggle('active');
 });
+
+window.addEventListener("scroll", function () {
+    var header = document.querySelector(".navbar");
+    console.log(header);
+    console.log(header.classList);
+    header.classList.toggle("sticky", window.scrollY > 0)
+})
